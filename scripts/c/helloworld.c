3cece9dd30679188be9f8786abb5ceaa3b57e70b
Reuben Antz <cheershrek@gmail.com>
2018-10-05 14:15:18 +0100

    added C language and C script (#5)
    * Added myself to contributers and my-cool-script to the scripts folder
    
    * added C laguage and C script

---
+++ scripts/c/helloworld.c
@@ -0,0 +1,17 @@
+/*
+
+     This program is my second push to this repo for the Hacktoberfest-2018
+
+     Written by Reuben Antz. (10-05-2018)
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+    // Print function
+    printf("\nHello World \n\tfrom Antz!\n");
+    return 0;
+}
+
